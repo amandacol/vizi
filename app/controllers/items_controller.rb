d00e7d5726abfb771f7a497ee32ef7b8392de0c1
matheusbarancelli <mbarancelli@hotmail.com>
2020-03-05 14:50:45 -0300

    working with itemcontroller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,25 @@
 class ItemsController < ApplicationController
+
+  def new
+    @items = Item.new
+    authorize @item
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @item = Item.new
+    authorize @item
+    @order.item = @item
+    @order.user = current_user
+    @order.date = Time.now
+    @order.save
+    redirect_to items_path, notice: "Successfully added to cart!"
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
