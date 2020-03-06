f770857c9f383fa46c3e7d417bf64049ba36b38b
Alana Nobrega <alana.almeida@ee.ufcg.edu.br>
2020-03-06 15:04:24 -0300

    Make a complite login.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, :surname, :address, presence: true, on: :update
 end
