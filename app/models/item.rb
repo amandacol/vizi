346de82e8185bfeaf7f624ad1d39a82363bf6743
Amanda Bezerra <colmenero88@bol.com.br>
2020-03-06 11:56:55 -0300

    fixing heroku again

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   pg_search_scope :search_by_name_and_description,
     against: [ :name, :description, :sport, :transaction_type ],
     using: {
-      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+      tsearch: { prefix: true }
     }
 
 end
