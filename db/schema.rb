bfb5eea69e2a3b3523f4410692bb3ef66b813f8c
Alana Nobrega <alana.almeida@ee.ufcg.edu.br>
2020-03-10 14:52:55 -0300

    make a table user_sport

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_204917) do
+ActiveRecord::Schema.define(version: 2020_03_10_174940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,15 @@ ActiveRecord::Schema.define(version: 2020_03_09_204917) do
     t.string "image"
   end
 
+  create_table "user_sports", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "sport_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["sport_id"], name: "index_user_sports_on_sport_id"
+    t.index ["user_id"], name: "index_user_sports_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -94,7 +103,6 @@ ActiveRecord::Schema.define(version: 2020_03_09_204917) do
     t.string "address"
     t.string "description"
     t.string "photo"
-    t.string "sport_tag"
     t.float "latitude"
     t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
@@ -114,6 +122,8 @@ ActiveRecord::Schema.define(version: 2020_03_09_204917) do
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
+  add_foreign_key "user_sports", "sports"
+  add_foreign_key "user_sports", "users"
   add_foreign_key "wishlists", "items"
   add_foreign_key "wishlists", "users"
 end
