e5e7745927331237dffa9c131c85dc8e9b09e46e
Estelle Quezel <estellequezel@gmail.com>
2020-03-12 16:35:53 -0300

    Amount coherent in stripe

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,5 +2,8 @@ class PaymentsController < ApplicationController
   def new
     @order = current_user.orders.where(state: 'pending').find(params[:order_id])
     authorize @order
+    if @order.item.transaction_type == "Rental"
+      @order.amount = @order.amount.to_i * (Date.parse(@order.rent_end_date) - Date.parse(@order.rent_start_date)).to_i
+    end
   end
 end
