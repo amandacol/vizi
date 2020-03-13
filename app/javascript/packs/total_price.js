1e8260b4d10133041ce297757a7c80cb77165a50
Estelle Quezel <estellequezel@gmail.com>
2020-03-13 09:26:28 -0300

    Show page button disabled when no dates selected

--- app/javascript/packs/total_price.js
+++ app/javascript/packs/total_price.js
@@ -1,13 +1,21 @@
 const totalPrice = () => {
-  const duration = ( new Date(range_start.value.split(' ')[2]) - new Date(range_start.value.split(' ')[0]) ) / 1000 / 60 / 60 / 24;
+  if (range_start.value != "") {
+    const duration = ( new Date(range_start.value.split(' ')[2]) - new Date(range_start.value.split(' ')[0]) ) / 1000 / 60 / 60 / 24;
 
-  const price = Number.parseFloat(document.getElementById('rent_price').innerText);
+    const price = Number.parseFloat(document.getElementById('rent_price').innerText);
 
-  const total_price = duration * price;
-  if (typeof(total_price)!= "NaN") {
-    const total = document.getElementById('total');
-    total.innerText = `Total: R$${total_price}`;
+    const total_price = duration * price;
+
+    if (total_price != NaN) {
+      const total = document.getElementById('total');
+      total.innerText = `Total: R$${total_price}`;
+    }
+
+    const button = document.getElementById( "btn-add-rent");
+    button.disabled = false;
+    button.classList.remove("disabled");
   }
+
 };
 
 export { totalPrice };
